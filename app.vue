451357a671e3ca77efff534eb30325cacf2cb5fb
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-02-10 13:13:07 +0530

    newone

---
+++ app.vue
@@ -0,0 +1,149 @@
+<template>
+ 
+    <div>
+      <TransitionRoot as="template" :show="sidebarOpen">
+        <Dialog class="relative z-50 lg:hidden" @close="sidebarOpen = false">
+          <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
+            <div class="fixed inset-0 bg-gray-900/80" />
+          </TransitionChild>
+  
+          <div class="fixed inset-0 flex">
+            <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
+              <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
+                <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+                  <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                    <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                      <span class="sr-only">Close sidebar</span>
+                      <i class="pi pi-times"></i>
+                    </button>
+                  </div>
+                </TransitionChild>
+                <!-- Sidebar component, swap this element with another sidebar if you like -->
+                <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+                 
+                  <nav class="flex flex-1 flex-col mt-3">
+                    <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                      <li>
+                        <ul role="list" class="-mx-2 space-y-1">
+                          <li v-for="item in navigation" :key="item.name">
+                            <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
+                              <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'size-6 shrink-0']" aria-hidden="true" />
+                              {{ item.name }}
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <div class="text-xs/6 font-semibold text-indigo-200">Your teams</div>
+                        <ul role="list" class="-mx-2 mt-2 space-y-1">
+                          <li v-for="team in teams" :key="team.name">
+                            <a :href="team.href" :class="[team.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
+                              <span class="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">{{ team.initial }}</span>
+                              <span class="truncate">{{ team.name }}</span>
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="mt-auto">
+                        <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
+                          Settings
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </Dialog>
+      </TransitionRoot>
+  
+      <!-- Static sidebar for desktop -->
+      <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+         
+          <nav class="flex flex-1 flex-col mt-3">
+            <ul role="list" class="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" class="-mx-2 space-y-1">
+                  <li v-for="item in navigation" :key="item.name">
+                    <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:bg-indigo-700 hover:text-white', 'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold']">
+                      <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'size-6 shrink-0']" aria-hidden="true" />
+                      {{ item.name }}
+                    </a>
+                  </li>
+                </ul>
+              </li>
+             
+            
+              <li class="mt-auto">
+                <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+  
+      <div class="lg:pl-72">
+        <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+          <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen = true">
+            <span class="sr-only">Open sidebar</span>
+           <i class="pi pi-bars"></i>
+          </button>
+  
+          <!-- Separator -->
+          <div class="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+  
+          <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+            <form class="grid flex-1 grid-cols-1" action="#" method="GET">
+              <input type="search" name="search" aria-label="Search" class="col-start-1 row-start-1 block size-full bg-white pl-8 text-base text-gray-900 outline-none placeholder:text-gray-400 sm:text-sm/6" placeholder="Search" />
+            </form>
+           
+          </div>
+        </div>
+  
+        <main class="py-2">
+          <div class="px-2">
+           <stocktable/>
+          </div>
+        </main>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import stocktable from '~/components/stocktable.vue'
+  import 'primeicons/primeicons.css'
+  import {
+    Dialog,
+    DialogPanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    TransitionChild,
+    TransitionRoot,
+  } from '@headlessui/vue'
+ 
+  import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
+  
+  const navigation = [
+    { name: 'Dashboard', href: '#',  current: true },
+    { name: 'Team', href: '#',  current: false },
+    { name: 'Projects', href: '#',  current: false },
+    { name: 'Calendar', href: '#', current: false },
+    { name: 'Documents', href: '#',  current: false },
+    { name: 'Reports', href: '#',  current: false },
+  ]
+ 
+  const userNavigation = [
+    { name: 'Your profile', href: '#' },
+    { name: 'Sign out', href: '#' },
+  ]
+  
+  const sidebarOpen = ref(false)
+  </script>
